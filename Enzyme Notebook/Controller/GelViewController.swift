745b9d74fac8197d827b1587a36cf9c0725e656a
Yashas Rao <yashas.rao24@gmail.com>
2018-03-22 19:31:24 -0700

    feat: Added camera, picker to gelVC

--- Enzyme Notebook/Controller/GelViewController.swift
+++ Enzyme Notebook/Controller/GelViewController.swift
@@ -12,9 +12,70 @@ import UIKit
 
 class GelViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var noPhotoLabel: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if imageView.image == nil {
+            noPhotoLabel.isHidden = false
+        } else {
+            noPhotoLabel.isHidden = true
+        }
+        
+        //let value = UIInterfaceOrientation.landscapeLeft.rawValue
+       // UIDevice.current.setValue(value, forKey: "orientation")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.isHidden = true
+        configureUI()
+    }
+    
+    func configureUI() {
         
+        self.navigationItem.title = "Gel Analayzer"
         
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(openCameraButton))
+        let activityButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(pick))
+        
+        self.navigationItem.rightBarButtonItem = cameraButton
+        self.navigationItem.leftBarButtonItem = activityButton
+    }
+    
+    @IBAction func openCameraButton(_ sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("No camera")
+        }
+    }
+    
+    func clearImage() {
+        if imageView.image != nil {
+            imageView.image = nil
+        }
     }
 }
+
+
+
+extension GelViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func pick(sourceType: UIImagePickerControllerSourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+
+
