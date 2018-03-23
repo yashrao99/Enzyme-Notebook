93af1be75efd43f620cdafc455e85fbb5cb7a107
Yashas Rao <yashas.rao24@gmail.com>
2018-03-22 18:45:08 -0700

    feat: gelViewer in newVC

---
+++ Enzyme Notebook/Controller/GelViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GelViewController.swift
+//  Enzyme Notebook
+//
+//  Created by Yash Rao on 3/22/18.
+//  Copyright © 2018 com.YashasRao99. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class GelViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
